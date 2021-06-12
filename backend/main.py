b023395c660e68ecac6582571ccc4406e0117a85
Alejandro <alejandro.deleon@utp.ac.pa>
2021-06-12 01:36:13 -0500

    Probando upload de imagenes

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,33 @@
 from typing import Optional
-
+import json
+from face_util import compare_faces, face_rec
 from sqlalchemy import create_engine, text
 import pymysql
+import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 db_engine = create_engine("mysql+pymysql://root:my-secret-pw@mariadb/", echo=True, future=True)
+
+@app.post('/face_match')
+async def face_match(file: list[UploadFile]=File(...)):
+    return len(file)
+    
+"""     if ('file1' in file) and ('file2' in file):        
+        file1 = files.get('file1')
+        file2 = files.get('file2')                         
+        ret = compare_faces(file1, file2)     
+        resp_data = {"match": bool(ret)} # convert numpy._bool to bool for json.dumps
+        return json.dumps(resp_data)       """   
+
+@app.post('./face_rec')
+async def face_recognition(file: UploadFile=File(...)):
+    if 'file' in files:
+        file = files.get('file')                          
+        name = face_rec(file)    
+        resp_data = {'name': name }
+        return json.dumps(resp_data)
 
 @app.get("/")
 def read_root():
@@ -19,3 +40,6 @@ def read_item(item_id: int, q: Optional[str] = None):
 with db_engine.connect() as db_conn:
     result = db_conn.execute(text("select 'hello world'"))
     print(result.all())
+
+# When debug = True, code is reloaded on the fly while saved
+app.run(host='0.0.0.0', port='5001', debug=True)
