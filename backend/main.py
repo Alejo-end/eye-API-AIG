f429679006c2e740515e0edb7210628483f47d14
Alejo-end <alejandroe.deleon@gmail.com>
2021-06-18 01:49:50 -0500

    agregando endpoints y funciones del API

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,62 @@
-from typing import Optional
+from typing import Optional, List, Tuple
 import json
-from face_util import compare_faces, face_rec
+from face_util import compare_faces, face_rec, image_has_face
 from sqlalchemy import create_engine, text
 import pymysql
 import uvicorn
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException, status
 
 app = FastAPI()
-db_engine = create_engine("mysql+pymysql://root:my-secret-pw@mariadb/", echo=True, future=True)
+""" db_engine = create_engine("mysql+pymysql://root:my-secret-pw@mariadb/", echo=True, future=True) """
+db_engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 
-@app.post('/compare_faces')
-async def face_match(file: list[UploadFile]=File(...)):
-    return len(file)
-    
-"""     if ('file1' in file) and ('file2' in file):        
-        file1 = files.get('file1')
-        file2 = files.get('file2')                         
-        ret = compare_faces(file1, file2)     
-        resp_data = {"match": bool(ret)} # convert numpy._bool to bool for json.dumps
-        return json.dumps(resp_data)       """   
 
-@app.post('./face_rec')
-async def face_recognition(file: list[UploadFile]=File(...)):
-    if 'file' in files:
-        file = files.get('file')                          
-        name = face_rec(file)    
-        resp_data = {'name': name }
-        return json.dumps(resp_data)
+@app.post("/compare_faces")
+async def face_match(files: List[UploadFile] = File(...)):
+    if len(files) != 2:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Tiene que ser dos imagenes")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    for image in files:
+        if image.content_type not in ("image/jpeg", "image/png"):
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST, detail="Deben ser imagenes en formato jpeg, o png."
+            )
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    resultado = compare_faces(files[0].file, files[1].file)
+    return {"resultado": str(resultado)}
+
+
+@app.post("/face_rec")
+async def face_recognition(file_rec: List[UploadFile] = File(...)):
+    if len(file_rec) != 1:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Debe subir 1 imagen")
+
+    for image in file_rec:
+        if image.content_type not in ("image/jpeg", "image/png"):
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST, detail="Deben ser imagenes en formato jpeg, o png."
+            )
+
+
+@app.post("/add_face")
+async def add_image_to_db(file: UploadFile = File(...)):
+    if file.content_type not in ("image/jpeg", "image/png"):
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Deben ser una imagen en formato jpeg, o png."
+        )
+
+    if not image_has_face(file):
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="No se detectan caras en esta imagen"
+        )
+
+    with db_engine.connect() as conn:
+        conn.execute()
+
+    return {"resultado": "Se ha guardado la imagen en la base de datos"}
+
 
 with db_engine.connect() as db_conn:
-    result = db_conn.execute(text("select 'hello world'"))
+    result = db_conn.execute(text("Bienvenido al API de reconocimiento facial."))
     print(result.all())
