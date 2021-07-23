bd645f69b2aebb3f7ed8cc76bd8466c6080fbdd6
Alejo-end <alejandroe.deleon@gmail.com>
2021-07-22 20:01:03 -0500

    Cambio en el navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -27,10 +27,10 @@ export default function NavBar(props: NavProps) {
             href="/docs"
             className="whitespace-nowrap text-base font-medium px-4 py-2 text-gray-500 hover:text-gray-900"
           >
-            Ver Documentación
+            Ver la documentación
           </a>
           <Link href="/login">
-            <a className="hidden ml-8 whitespace-nowrap md:inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-800 hover:bg-blue-900">
+            <a className="hidden ml-8 whitespace-nowrap items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-800 hover:bg-blue-900">
               Iniciar Sesión
             </a>
           </Link>
